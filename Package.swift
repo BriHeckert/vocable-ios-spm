384aef4f5aea71d595877091229f9b3e3a4dec1c
Chris Stroud <chris.stroud@willowtreeapps.com>
2021-02-09 12:24:00 -0500

    Update framework for iOS 14 and declare platform minimum target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "VocableCore",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "VocableCore",
