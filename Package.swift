3a102521dc93fa6a8dcad8bb9209074a44a121af
Bri Heckert <bri.heckert@willowtreeapps.com>
2024-06-05 13:21:19 -0400

    visionOS addition

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "VocableCore",
+    name: "VocableCore2",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .visionOS(.v1)
     ],
     products: [
         .library(
